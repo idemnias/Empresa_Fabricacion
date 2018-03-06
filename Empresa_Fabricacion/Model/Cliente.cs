42b6f921613612adc03ddb478ee69e5ccfda922c
idemnias <idemnias@gmail.com>
2018-03-06 19:53:44 +0100

    1 subida

---
+++ Empresa_Fabricacion/Model/Cliente.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Empresa_Fabricacion.Model
+{
+    [Table("Clientes")]
+    public class Cliente
+    {
+        public int ClienteId;
+
+
+    }
+}
